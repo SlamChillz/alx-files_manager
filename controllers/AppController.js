1a8796ff43894e9720397c253bc74d318ce81d02
slamchillz <mendyslam@gmail.com>
2023-03-09 04:48:15 +0100

    removed return statement

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,11 +5,11 @@ import dbClient from '../utils/db';
 import redisCleint from '../utils/redis';
 
 const getStatus = (req, res) => {
-  return res.json({ redis: redisCleint.isAlive(), db: dbClient.isAlive() });
+  res.status(200).json({ redis: redisCleint.isAlive(), db: dbClient.isAlive() });
 };
 
 const getStats = async (req, res) => {
-  return res.json({ users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
+  res.status(200).json({ users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
 };
 
 module.exports = { getStats, getStatus };
