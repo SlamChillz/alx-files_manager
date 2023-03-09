93e8fa947e35424c5be11a7a9e243bcb52864603
slamchillz <mendyslam@gmail.com>
2023-03-09 04:21:40 +0100

    using return statement

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,11 +5,11 @@ import dbClient from '../utils/db';
 import redisCleint from '../utils/redis';
 
 const getStatus = (req, res) => {
-  res.json({ redis: redisCleint.isAlive(), db: dbClient.isAlive() });
+  return res.json({ redis: redisCleint.isAlive(), db: dbClient.isAlive() });
 };
 
 const getStats = async (req, res) => {
-  res.json({ users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
+  return res.json({ users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
 };
 
 module.exports = { getStats, getStatus };
