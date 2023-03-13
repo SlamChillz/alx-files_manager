d3fc5e654eb4e5f5028cf815c9cb00d2ccd62698
slamchillz <mendyslam@gmail.com>
2023-03-13 16:15:15 +0100

    worker process for converting an image file to thumbnails

---
+++ worker.js
@@ -0,0 +1,58 @@
+/**
+ * A worker that processes the fileQueue
+ * It fecthes the file from the mongodb database
+ * Creates 3 thumbnails of width size 500, 250 and 100
+ */
+import fs from 'fs';
+import Queue from 'bull';
+import { ObjectId } from 'mongodb';
+import imageThumbnail from 'image-thumbnail';
+import dbClient from './utils/db';
+
+const fileQueue = new Queue('fileQueue');
+
+async function thumbnail(path, width) {
+  try {
+    const newPath = `${path}_${width}`;
+    const thumbnail = await imageThumbnail(path, { width });
+    fs.writeFileSync(newPath, thumbnail);
+    return Promise.resolve(width);
+  } catch (error) {
+    console.log(error);
+    return Promise.reject(width);
+  }
+}
+
+async function unlink(path, widths) {
+  for (const width of widths) {
+    fs.unlink(`${path}_${width.value}`, (err) => {
+      if (err) console.log(`Error encountered while removing: ${path}_${width.value}`);
+    });
+  }
+}
+
+fileQueue.process((job, done) => {
+  const { userId, fileId } = job.data;
+  if (userId === undefined) done(new Error('Missing userId'));
+  if (fileId === undefined) done(new Error('Missing fileId'));
+  dbClient.findOne('files', { _id: ObjectId(fileId), userId: ObjectId(userId) })
+    .then((file) => {
+      if (!file) throw new Error('File not found');
+      const path = file.localPath;
+      Promise.allSettled([thumbnail(path, 500), thumbnail(path, 250), thumbnail(path, 100)])
+        .then((results) => {
+          const fails = results.filter((r) => r.status === 'rejected');
+          if (fails.length > 0) {
+            const success = results.filter((r) => r.status === 'fulfilled');
+            unlink(path, success);
+            done(new Error('Could not create thumbnails successfully!'));
+          }
+          console.log('All thumbnails created!');
+          done();
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      done(err);
+    });
+});
